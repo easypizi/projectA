f20af32829313d43d6e7b64f2917d7a5edf8bb0c
Evan Tolstof <itolstof@gmail.com>
2021-06-07 13:22:24 +0300

    change port

--- server/config.js
+++ server/config.js
@@ -6,7 +6,7 @@ module.exports = {
     },
   },
   staticFolder: "static",
-  defaultPort: 3000,
+  defaultPort: 1522,
   cacheTTL: 30000,
   sessionSecret: "VI_VERI_VENIVERSUM_VIVUS_VICI",
   langs: ["en"],
